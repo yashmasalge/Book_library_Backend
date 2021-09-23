2b006b9a44c525325c276aa80f02e5dff00eea3a
yashmasalge <myash2101@gmail.com>
2021-09-23 21:31:49 +0530

    POST Methods

--- index.js
+++ index.js
@@ -1,37 +1,115 @@
-const data = require("./database/index.js");
-// console.log(data);
+// MAIN BACKEND FILE
+
+const db = require("./database/index");
+// console.log(db.books);
+// console.log(db.authors);
+// console.log(db.publications);
 
 const express = require("express");
 
 const app = express();
 
-app.get("/",(req,res) => {
-    const getAllBooks = data.books;
-    return res.json(getAllBooks);
-})
 
-app.get("/book/:isbn",(req,res) => {
-    console.log(req.params);
+// using json for post
+app.use(express.json());
+
+// http://localhost:3000/
+app.get("/", (req, res) => {
+    return res.json({"WELCOME": `to my Backend Software for the Book Company`});
+});
+
+// http://localhost:3000/books
+app.get("/books", (req, res) => {
+    const getAllBooks = db.books;
+    return res.json(getAllBooks);
+});
+
+// http://localhost:3000/book-isbn/12345Two
+app.get("/book-isbn/:isbn", (req, res) => {
+    // console.log(req.params);
     const {isbn} = req.params;
-    console.log(isbn)
-    const getSpecificBook = data.books.filter((book) => book.ISBN == isbn);
-    if(getSpecificBook.length == 0){
-        return res.json("Error : No such Book Present");
+    // console.log(isbn);
+    const getSpecificBook = db.books.filter((book) => book.ISBN === isbn);
+    // console.log(getSpecificBook);
+    // console.log(getSpecificBook.length);
+    if(getSpecificBook.length===0) {
+        return res.json({"error": `No Book found for the ISBN of ${isbn}`});
     }
     return res.json(getSpecificBook[0]);
-})
+});
 
-app.get("/book-category/:category",(req,res) => {
-    console.log(req.params);
+// http://localhost:3000/book-category/programming
+app.get("/book-category/:category", (req, res) => {
+    // // console.log(req.params);
     const {category} = req.params;
-    console.log(category)
-    const getSpecificCategory = data.books.filter((book) => book.category.includes(category) == category);
-    if(getSpecificCategory.length == 0){
-        return res.json("Error : No such Book Present");
+    // // console.log(isbn);
+    const getSpecificBooks = db.books.filter((book) => book.category.includes(category));
+    // // console.log(getSpecificBook);
+    // // console.log(getSpecificBook.length);
+    if(getSpecificBooks.length===0) {
+        return res.json({"error": `No Books found for the category of ${category}`});
     }
-    return res.json("getSpecificCategory.includes(book.category)");
+    return res.json(getSpecificBooks);
+});
+
+// http://localhost:3000/authors
+app.get("/authors", (req, res) => {
+    const getAllAuthors = db.authors;
+    return res.json(getAllAuthors);
+});
+
+// http://localhost:3000/author-id/1
+app.get("/author-id/:id", (req, res) => {
+    // console.log(req.params);
+    let {id} = req.params;
+    id = Number(id);
+    // console.log(id);
+    const getSpecificAuthor = db.authors.filter((author) => author.id === id);
+    // console.log(getSpecificAuthor);
+    // console.log(getSpecificAuthor.length);
+    if(getSpecificAuthor.length===0) {
+        return res.json({"error": `No Author found for the id of ${id}`});
+    }
+    return res.json(getSpecificAuthor[0]);
+});
+
+// http://localhost:3000/author-isbn/12345Two
+app.get("/author-isbn/:isbn", (req, res) => {
+   
+});
+
+// http://localhost:3000/publications
+app.get("/publications", (req, res) => {
+    const getAllPublications = db.publications;
+    return res.json(getAllPublications);
+});
+
+// http://localhost:3000/publication-isbn/12345Two
+app.get("/publication-isbn/:isbn", (req, res) => {
+   
+});
+
+// Posting a new book
+app.post("/addBook",(req,res) => {
+    console.log(req.body);
+    db.books.push(req.body);
+    return res.json(db.books)
 })
 
-app.listen(300,() =>{
-    console.log("Server started")
+// Posting new publication
+app.post("/addpublications",(req,res) => {
+    console.log(req.body);
+    db.publications.push(req.body);
+    return res.json(db.publications)
 })
+
+// Posting new Author
+app.post("/addauthor",(req,res) => {
+    console.log(req.body);
+    db.authors.push(req.body);
+    return res.json(db.authors)
+})
+
+app.listen(3000, () => {
+    console.log("MY EXPRESS APP IS RUNNING.....")
+});
